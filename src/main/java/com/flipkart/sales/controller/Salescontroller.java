48125034f4642e6c72fc6c6658d763d68fdf8050
shahimsha786 <shahimsha786@gmail.com>
2025-07-15 18:25:36 +0530

    commited messages

--- src/main/java/com/flipkart/sales/controller/Salescontroller.java
+++ src/main/java/com/flipkart/sales/controller/Salescontroller.java
@@ -12,7 +12,7 @@ public class Salescontroller {
 	
 	@GetMapping("/sale/{imei}")
 	public ResponseEntity<String> postSale(@PathVariable("imei") String imei){
-		String message="posted sale for this device"+imei+"by shahimsha successfully";
+		String message="posted sale for this device"+imei+"by mdshahimsha successfully";
 		System.out.println(message);
 		return ResponseEntity.ok(message);
 	}
